b0a179aff2f2c0004c55578d0adbc9683de8e5dd
natalia <natalia.ar969@gmail.com>
2021-09-19 20:09:52 -0600

    listas de cantones (por ahorita en el main)

--- src/payroll/Application.java
+++ src/payroll/Application.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package payroll;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import payroll.presentation.principal.View;
 /**
  *
@@ -12,6 +15,9 @@ import payroll.presentation.principal.View;
  *         Michelle Delgado Meneses
  * 
  */
+
+
+
 public class Application {
 
     /**
@@ -19,12 +25,44 @@ public class Application {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-                
+        
+        List<String> cantonesSJ = new ArrayList<>();
+        Collections.addAll(cantonesSJ, "San Jose", "Escazu", "Desamparados", 
+                "Puriscal", "Tarrazu", "Aserri", "Mora", "Goicoeches", "Santa Ana", "Alajuelita", "Coronoado",
+                "Acosta", "Tibaa", "Moravia", "Montes de Oca", "Turrubares", "Dota", "Perez Zeledon", "León Cortés Castro",
+                "Alajuelita");
+        
+        List<String> cantonesAlajuela = new ArrayList<>();
+        Collections.addAll(cantonesAlajuela, "Alajuela", "San Ramón", "Grecia", "San Mateo", 
+                "Atenas", "Naranjo", "Palmares", "Poás", "Orotina", "San Carlos", 
+                "Zarcero", "Valverde Vega", "Upala", "Los Chiles", "Guatuso");
+
+        List<String> cantonesCartago = new ArrayList<>();
+        Collections.addAll(cantonesCartago, "Cartago", "Paraíso", "La Unión", 
+                "Jiménez", "Turrialba", "Alvarado", "Oreamuno", "El Guarco");
+        
+        List<String> cantonesHeredia = new ArrayList<>();
+        Collections.addAll(cantonesHeredia, "Heredia", "Barva", "Santo Domingo", "Santa Bárbara", 
+                "San Rafael", "San Isidro", "Belén", "Flores", "San Pablo", "Sarapiquí");
+        
+        List<String> cantonesGuanacaste = new ArrayList<>();
+        Collections.addAll(cantonesGuanacaste, "Liberia", "Nicoya", "Santa Cruz", "Bagaces", "Carrillo", "Cañas", 
+                "Abangares", "Tilarán", "Nandayure", "La Cruz", "Hojancha");
+        
+        List<String> cantonesPuntarenas = new ArrayList<>();
+        Collections.addAll(cantonesPuntarenas, "Puntarenas", "Esparza", "Buenos Aires", 
+                "Montes de Oro", "Osa", "Aguirre", "Golfito", "Coto Brus", "Parrita", "Corredores", 
+                "Garabito", "Monteverde");
+        
+        List<String> cantonesLimon = new ArrayList<>();
+        Collections.addAll(cantonesLimon, "Limón", "Pococí", "Siquirres", "Talamanca", "Matina", "Guacimo");
+        
+        
+        
+        
+        
         View v = new View();
         v.setVisible(true);
     }
     
 }
- //prueba a ver si funciona
-//hola
-//prueba desde aca
