fabbec1c0c605e7c2885142646eb58a885f7143e
natalia <natalia.ar969@gmail.com>
2021-09-28 10:49:55 -0600

    sin cambios

--- src/payroll/logic/Canton.java
+++ src/payroll/logic/Canton.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlIDREF;
 /**
  *
  * @author Michelle
